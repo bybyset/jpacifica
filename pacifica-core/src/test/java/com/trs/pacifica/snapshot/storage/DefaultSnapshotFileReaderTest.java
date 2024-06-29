15d36fa4961f0343b14c48d37ffac52d770a4bf9
FengFeng Cheng <89673223+JavaCoderCff@users.noreply.github.com>
2024-06-29 16:30:44 +0800

    DefaultFileReader  test case

--- pacifica-core/src/test/java/com/trs/pacifica/snapshot/storage/DefaultSnapshotFileReaderTest.java
+++ pacifica-core/src/test/java/com/trs/pacifica/snapshot/storage/DefaultSnapshotFileReaderTest.java
@@ -17,13 +17,76 @@
 
 package com.trs.pacifica.snapshot.storage;
 
+import com.trs.pacifica.fs.FileReader;
+import com.trs.pacifica.test.BaseStorageTest;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class DefaultSnapshotFileReaderTest {
-    DefaultSnapshotFileReader defaultSnapshotFileReader = new DefaultSnapshotFileReader("snapshotPath");
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class DefaultSnapshotFileReaderTest extends BaseStorageTest {
+
+
+    DefaultSnapshotFileReader defaultSnapshotFileReader;
+
+    private String snapshotPath;
+
+    private String filename;
+
+    private byte[] fileContent;
+
+    @BeforeEach
+    @Override
+    public void setup() throws Exception {
+        super.setup();
+        mockForTest();
+        this.defaultSnapshotFileReader = new DefaultSnapshotFileReader(this.snapshotPath);
+    }
+
+    @AfterEach
+    @Override
+    public void shutdown() throws Exception {
+        super.shutdown();
+    }
+
+
+    private void mockForTest() throws IOException {
+        this.filename = "test1";
+        this.snapshotPath = this.path + File.separator + "snapshot_1133";
+        File snapshotDir = new File(snapshotPath);
+        snapshotDir.mkdir();
+        File snapshotFile = new File(snapshotDir, this.filename);
+        this.fileContent = "this is test content for DefaultSnapshotFileReader".getBytes("utf-8");
+        FileUtils.writeByteArrayToFile(snapshotFile, fileContent);
+
+    }
+
 
     @Test
-    void testRead() {
+    void testRead() throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(this.fileContent.length);
+        int len = this.defaultSnapshotFileReader.read(buffer, this.filename, 0, this.fileContent.length);
+        Assertions.assertEquals(this.fileContent.length, len);
+        Assertions.assertArrayEquals(this.fileContent, buffer.array());
+    }
+
+    @Test
+    void testReadMultiBatch() throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(this.fileContent.length);
+        int offset = 0;
+        int batch = 5;
+        int len = -1;
+        while ((len = this.defaultSnapshotFileReader.read(buffer, this.filename, offset, batch)) != DefaultSnapshotFileReader.EOF) {
+            offset += len;
+        }
+        Assertions.assertEquals(FileReader.EOF, len);
+        Assertions.assertEquals(this.fileContent.length, buffer.limit());
+        Assertions.assertArrayEquals(this.fileContent, buffer.array());
     }
 
 
